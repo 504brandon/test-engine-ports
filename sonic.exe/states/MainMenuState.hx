d56f86a40a8020a1b961cd440a7a11245012bc39
504brandon <mb.bot.official@gmail.com>
2023-01-29 20:17:11 -0600

    saster?!?!?!?!??!?!?!?!??!?!?!!??!?!?!?

---
+++ sonic.exe/states/MainMenuState.hx
@@ -0,0 +1,28 @@
+import handlers.CoolUtil;
+
+function create() {
+	optionShit = ['story mode', 'encore', 'freeplay', 'sound test', 'options', 'extras'];
+}
+
+function createBellowItems() {
+	var bg2:FlxSprite = new FlxSprite();
+	bg2.frames = Files.sparrowAtlas('menus/mainmenu/Main_Menu_Spritesheet_Animation');
+	bg2.animation.addByPrefix('a', 'BG instance 1');
+	bg2.animation.play('a', true);
+	bg2.scrollFactor.x = 0;
+	bg2.scrollFactor.y = 0;
+	bg2.updateHitbox();
+	bg2.screenCenter();
+	bg2.antialiasing = true;
+	add(bg2);
+}
+
+function updatePost(elapsed) {
+	if (FlxG.keys.justPressed.TAB)
+		CoolUtil.switchToCustomState('teststate');
+
+	for (i in 0...optionShit.length) {
+		var offset:Float = 120 - (Math.max(optionShit.length, 2) - 3) * 30;
+		menuItems.members[i].setPosition(554 + 70 * i, (i * 100) + offset);
+	}
+}
---
+++ sonic.exe/states/TitleState.hx
@@ -0,0 +1,37 @@
+var textTitle:FlxSprite;
+var bf:FlxSprite;
+
+function createPost() {
+	bg = new FlxSprite(0, 0);
+	bg.frames = Files.sparrowAtlas('menus/titlescreen/NewTitleMenuBG');
+	bg.animation.addByPrefix('idle', "TitleMenuSSBG instance 1", 24);
+	bg.animation.play('idle');
+	bg.scale.x = 3;
+	bg.scale.y = 3;
+	bg.antialiasing = true;
+	bg.updateHitbox();
+	bg.screenCenter();
+	add(bg);
+
+	logoCool = new FlxSprite(0, 0);
+	logoCool.loadGraphic(Files.image('menus/titlescreen/logo'));
+	logoCool.antialiasing = true;
+	logoCool.scale.set(0.5, 0.5);
+	logoCool.screenCenter();
+	add(logoCool);
+
+	textTitle = new FlxSprite(0, 0);
+	textTitle.frames = Files.sparrowAtlas('menus/titlescreen/titleEnterNEW');
+	textTitle.animation.addByPrefix('idle', "Press Enter to Begin instance 1", 24);
+	textTitle.animation.addByPrefix('press', "ENTER PRESSED instance 1", 24, false);
+	textTitle.antialiasing = true;
+	textTitle.animation.play('idle');
+	textTitle.updateHitbox();
+	textTitle.screenCenter();
+	add(textTitle);
+}
+
+function updatePost() {
+	if (controls.ACCEPT)
+		textTitle.animation.play('press', true);
+}
